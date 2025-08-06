3cbbba12ae2ea2fd86d8412fd784ee571fec138b
luciclei-dev <lucicleioliveira8@gmail.com>
2025-08-05 21:15:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
 
-  document.querySelector('.leao').style.transform = `translate(-50%) translateX(${scrollY * 0.2}px)`;
+  document.querySelector('.leao').style.transform = `translate(-50%) translatex(${scrollY * 0.2}px)`;
   document.querySelector('.onca').style.transform = `translate(-50%) translateX(${scrollY * -0.3}px)`;
   document.querySelector('.gurila').style.transform = `translate(-50%) translateX(${scrollY * 0.2}px)`;
   document.querySelector('.elefante').style.transform = `translate(-50%) translateX(${scrollY * -0.3}px)`;
+   
+  //const imagens = document.querySelectorAll('.section-imagem');
+  //const paragrafos = document.querySelectorAll('.paragrafo');
 
-  document.querySelector('.section-imagem').style.transform = `translate(-50%) translateX(${scrollY * 0.3}px)`;
-  document.querySelector('.paragrafo').style.transform = `translate(50%) translateX(${scrollY * -0.3}px)`;
+  imagens.forEach((img) => {
+    img.style.transform = `translate(-50%) translateX(${scrollY * 0.4}px)`;
+  });
+
+  paragrafos.forEach((p) => {
+    p.style.transform = `translate(50%) translateX(${scrollY * -0.4}px)`
+  });
 });
+
+
