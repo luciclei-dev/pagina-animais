5dfc53b4536e3fa1c132f2beaa9c4310db20d682
luciclei-dev <lucicleioliveira8@gmail.com>
2025-08-08 20:35:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.addEventListener('scroll', () => {
   const scrollY = window.scrollY;
 
-  document.querySelector('.leao').style.transform = `translate(-50%) translatex(${scrollY * 0.2}px)`;
-  document.querySelector('.onca').style.transform = `translate(-50%) translateX(${scrollY * -0.3}px)`;
-  document.querySelector('.gurila').style.transform = `translate(-50%) translateX(${scrollY * 0.2}px)`;
-  document.querySelector('.elefante').style.transform = `translate(-50%) translateX(${scrollY * -0.3}px)`;
+  document.querySelector('.leao').style.transform = `translate(-50%) translatex(${scrollY * 0.4}px)`;
+  document.querySelector('.onca').style.transform = `translate(-50%) translateX(${scrollY * -0.5}px)`;
+  document.querySelector('.gurila').style.transform = `translate(-50%) translateX(${scrollY * 0.4}px)`;
+  document.querySelector('.elefante').style.transform = `translate(-50%) translateX(${scrollY * -0.5}px)`;
    
   //const imagens = document.querySelectorAll('.section-imagem');
   //const paragrafos = document.querySelectorAll('.paragrafo');
